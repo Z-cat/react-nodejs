9bc899062aabb2d28ed6b3b80224c0455ca77081
ZCAT <544520183@qq.com>
2022-06-15 22:52:05 +0800

    login接口初步实现

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+const url = require('url');
+const data = require('./login.json')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log(url.parse(req.url,true).query.username);
+  const {username,password} =url.parse(req.url,true).query
+  if(username&&password){
+    res.send(data)
+  }else{
+    res({
+      "code":"error", 
+      "message":"不能为空",
+      "data": {}
+    })
+  }
+  
 });
 
 module.exports = router;
